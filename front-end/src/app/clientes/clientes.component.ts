161a37b6dc17f7aa0fbe406f28b03cb822ecac7e
Fabio Amaral <fabio.amaral@vetta.digital>
2022-02-03 19:38:01 -0300

    (front-end): adds customer register page

--- front-end/src/app/clientes/clientes.component.ts
+++ front-end/src/app/clientes/clientes.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CustomerService } from '../services/customer.service';
 
 @Component({
   selector: 'app-clientes',
@@ -7,9 +10,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientesComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private customerService: CustomerService,
+    private router: Router
+    ) { }
 
-  ngOnInit(): void {
+    ngOnInit(): void {
+      if(this.customerService.customerPhoneNumber)
+        this.formRegisterCustomer.get('phoneNumber')?.setValue(this.customerService.customerPhoneNumber);
+    }
+
+  formRegisterCustomer = new FormGroup({
+    name: new FormControl('',[
+      Validators.required,
+      Validators.maxLength(120)]),
+    phoneNumber: new FormControl('',[
+      Validators.required]),
+    cep: new FormControl(''),
+    street: new FormControl('',[
+      Validators.required]),
+    houseNumber: new FormControl('',[
+      Validators.required]),
+    neighborhood: new FormControl('',[
+      Validators.required]),
+    cidade: new FormControl('',[
+      Validators.required,
+      Validators.maxLength(50)]),
+    additionalInfo: new FormControl(''),
+    reference: new FormControl('')
+  });
+
+  consultaCEP() {
+
+  }
+
+  registerCustomer() {
+
   }
 
 }
